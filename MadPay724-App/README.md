441249d7cbff706e218bfed8db41b8d01a313378
masoud <masoudtahmasebi@hotmail.com>
2020-10-30 13:59:37 +0330

    add template admin and app

--- MadPay724-App/README.md
+++ MadPay724-App/README.md
@@ -1,6 +1,6 @@
 # MadPay724App
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.1.0.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.3.9.
 
 ## Development server
 
--- MadPay724-App/e2e/src/app.e2e-spec.ts
+++ MadPay724-App/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('MadPay724-App app is running!');
+    expect(page.getTitleText()).toEqual('Welcome to MadPay724-App!');
   });
 
   afterEach(async () => {
